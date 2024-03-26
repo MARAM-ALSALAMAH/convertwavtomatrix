9a7dd8ad54a72c5e9e791d5f0c75f4a1b5f380f3
MARAM-ALSALAMAH <92258515+MARAM-ALSALAMAH@users.noreply.github.com>
2024-03-26 03:23:16 +0300

    Create ferst.py

---
+++ ferst.py
@@ -0,0 +1,11 @@
+!pip install librosa
+import librosa
+import numpy as np
+import sounddevice as sd
+
+# قراءة الملف الصوتي
+audio_file = 'path_to_audio_file.wav'  # يجب استبدال path_to_audio_file بمسار الملف الفعلي
+audio, sr = librosa.load(audio_file, sr=None)  # sr=None يحافظ على معدل العينات الأصلي
+
+# إنشاء مصفوفة الصوت
+audio_matrix = librosa.feature.melspectrogram(y=audio, sr=sr)

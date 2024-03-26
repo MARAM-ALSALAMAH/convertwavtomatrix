fcaa86fe5266c74727c8ce5c18711559adea5d44
MARAM-ALSALAMAH <92258515+MARAM-ALSALAMAH@users.noreply.github.com>
2024-03-26 03:25:14 +0300

    Update ferst.py

--- ferst.py
+++ ferst.py
@@ -4,7 +4,7 @@ import numpy as np
 import sounddevice as sd
 
 # قراءة الملف الصوتي
-audio_file = 'path_to_audio_file.wav'  # يجب استبدال path_to_audio_file بمسار الملف الفعلي
+audio_file = 'CristianoRonaldo.wav'  # يجب استبدال path_to_audio_file بمسار الملف الفعلي
 audio, sr = librosa.load(audio_file, sr=None)  # sr=None يحافظ على معدل العينات الأصلي
 
 # إنشاء مصفوفة الصوت
